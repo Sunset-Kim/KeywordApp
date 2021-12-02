136c4da762d4efda20513424c4c5c1f013c06d79
Sunset-Kim <77092632+Sunset-Kim@users.noreply.github.com>
2021-12-02 20:06:33 +0900

    💄  add theme color

---
+++ style/theme.js
@@ -0,0 +1,26 @@
+
+const darkTheme = {
+  red: {
+    main: "#e74c3c"
+  },
+  primary: {
+    main: "#f1c40f",
+    dark: "#f39c12"
+  },
+  background: "#2c3e50",
+  foreground: "#ecf0f1"
+}
+const lightTheme = {
+  red: {
+    main: "#e74c3c"
+  },
+  primary: {
+    main: "#3498db",
+    dark: "#2980b9"
+  },
+  background: "#ecf0f1",
+  foreground: "#2c3e50"
+}
+
+
+export { darkTheme, lightTheme }
