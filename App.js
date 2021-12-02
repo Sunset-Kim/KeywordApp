721bc22684d5a14a019ce7339d09e950c33b348c
Sunset-Kim <77092632+Sunset-Kim@users.noreply.github.com>
2021-12-02 19:03:08 +0900

    🎉  start build app

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Text, Animated, TouchableOpacity, Pressable, Dimensions, PanResponder } from 'react-native';
+import { Easing } from 'react-native-reanimated';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
 
 export default function App() {
+  // panResponder
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+
+    </Container >
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
